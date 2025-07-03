640b7a849d860c3885b5a13ec145854570670c16
rodgers Kipkurui <korirrodgers103@gmail.com>
2025-07-03 17:45:14 +0300

    `feat: updated client code with lucide-react, removed auth pages, and updated products page`

--- client/src/pages/admin/UsersPage.tsx
+++ client/src/pages/admin/UsersPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Edit, Trash2, UserPlus } from "lucide-react";
 import AdminSidebar from "../../components/admin/AdminSidebar";
 
